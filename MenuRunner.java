3a1640b5fe159c6c58c7d712c31b91e5855ad483
unknown <sahoisagar70@gmail.com>
2020-04-22 11:51:28 +0530

    modify 2

--- MenuRunner.java
+++ MenuRunner.java
@@ -4,16 +4,22 @@ public class MenuRunner
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		System.out.print("Enter the 1st number : ");
+
 		int number1=scanner.nextInt();
+
 		System.out.print("Enter the 2nd number : ");
+
 		int number2=scanner.nextInt();
+
 		System.out.println("1.add");
 		System.out.println("2.subtract");
 		System.out.println("3.multiplication");
 		System.out.println("4.division");
 		System.out.print("Enter your choice :");
+
 		int choice=scanner.nextInt();
 		int result=0;
+
 		if(choice==1)
 		{
 			result=number1+number2;
@@ -34,6 +40,8 @@ public class MenuRunner
 		{
 			System.out.println("invalid input");
 		}
+
 		System.out.println("The result is :"+ result);
+		
 	}
 }
